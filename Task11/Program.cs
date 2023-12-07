454544f8d961e6ac193a6600deb1dbb39dc97520
Roman <user@email.com>
2023-12-08 00:31:15 +0500

    added task11

---
+++ Task11/Program.cs
@@ -0,0 +1,65 @@
+﻿// Создать класс, описывающий понятие работник, со свойствами:
+
+// фамилия;
+// стаж;
+// часовая заработная плата;
+// количество отработанных часов.
+// C помощью метода реализовать ввод данных работника с клавиатуры. Рассчитать с помощью методов заработную плату, 
+// за отработанное время, и премию, размер которой определяется в зависимости от стажа 
+// (при стаже до 1 года 0%, до 3 лет 5%, до 5 лет 8%, свыше 5 лет 15%).
+// С помощью метода печати, реализовать вывод информации о работнике на экран. Предусмотреть метод для записи в файл данных о работнике.
+Console.WriteLine("Enter workers name : ");
+string sr = Console.ReadLine();
+Console.WriteLine("Enter workers expirience : ");
+double ex = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter workers hourly wag :");
+int hg = int.Parse(Console.ReadLine());
+Console.WriteLine($"Enter how much {sr} has worked :");
+int hw = int.Parse(Console.ReadLine());
+
+
+Worker nom1 = new (sr,ex,hg,hw);
+Console.WriteLine();
+nom1.Print();
+nom1.ReadToFile();
+
+class Worker 
+{
+    public string Surname;
+    public double Expirience;
+    public int HourlyWag;
+    public int HoursWorked;
+    public int Salary;
+    public int Premy;
+
+    public Worker(string sur, double exp, int hag, int how)
+    {
+        Surname = sur;
+        Expirience = exp;
+        HourlyWag = hag;
+        HoursWorked = how;
+        Salary = hag * how;
+        if(Expirience < 1) Premy = 0;
+        if(Expirience > 1 && Expirience < 3 ) Premy = Salary / 100 * 5;
+        if(Expirience > 3 && Expirience < 5 ) Premy = Salary / 100 * 8;
+        if(Expirience > 5) Premy = Salary / 100 * 15;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Surname : {Surname}");
+        Console.WriteLine($"Expirience is : {Expirience}");
+        Console.WriteLine($"Hourly wage is: {HourlyWag}");
+        Console.WriteLine($"{Surname} has worked {HoursWorked}");
+        Console.WriteLine($"Salary is : {Salary}");
+        Console.WriteLine($"Premy is : {Premy}");
+    }
+
+    public void ReadToFile() {
+        string Info = "Surname is " + Surname + "'\n'Expirience is " + Expirience + "'\n'HourlyWag is " 
+                      + HourlyWag + "'\n'HoursWorked is " + HoursWorked 
+                      + "'\n'Salary is " + Salary + "'\n'Premy is " + Premy;
+        File.WriteAllText(@"C:\\Users\РомаЛиля\Desktop\C#Task11\test.txt",Info);
+    }
+}
+
