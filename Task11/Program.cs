a765d52ecf9c7d3a14d4a6d58e09c2650d308f16
Roman <user@email.com>
2023-12-08 20:34:40 +0500

    added remarks

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -8,7 +8,8 @@
 // за отработанное время, и премию, размер которой определяется в зависимости от стажа 
 // (при стаже до 1 года 0%, до 3 лет 5%, до 5 лет 8%, свыше 5 лет 15%).
 // С помощью метода печати, реализовать вывод информации о работнике на экран. Предусмотреть метод для записи в файл данных о работнике.
-Console.WriteLine("Enter workers name : ");
+
+Console.WriteLine("Enter workers surname : ");
 string sr = Console.ReadLine();
 Console.WriteLine("Enter workers expirience : ");
 double ex = double.Parse(Console.ReadLine());
@@ -18,13 +19,14 @@ Console.WriteLine($"Enter how much {sr} has worked :");
 int hw = int.Parse(Console.ReadLine());
 
 
-Worker nom1 = new (sr,ex,hg,hw);
+Worker nom1 = new (sr,ex,hg,hw); // Cоздание обьекта класса
 Console.WriteLine();
-nom1.Print();
-nom1.ReadToFile();
+nom1.Print(); // вызов метода печати
+nom1.RecordToFile(); // вызов метода записи
 
-class Worker 
+class Worker // Определение класса 
 {
+    // поля
     public string Surname;
     public double Expirience;
     public int HourlyWag;
@@ -32,20 +34,20 @@ class Worker
     public int Salary;
     public int Premy;
 
-    public Worker(string sur, double exp, int hag, int how)
+    public Worker(string sur, double exp, int hag, int how) // рассчёт зп и премии
     {
         Surname = sur;
         Expirience = exp;
         HourlyWag = hag;
         HoursWorked = how;
         Salary = hag * how;
-        if(Expirience < 1) Premy = 0;
-        if(Expirience > 1 && Expirience < 3 ) Premy = Salary / 100 * 5;
-        if(Expirience > 3 && Expirience < 5 ) Premy = Salary / 100 * 8;
-        if(Expirience > 5) Premy = Salary / 100 * 15;
+        if(Expirience < 1) Premy = 0; // если стаж < года
+        if(Expirience > 1 && Expirience < 3 ) Premy = Salary / 100 * 5; // если стаж > года но < 3
+        if(Expirience > 3 && Expirience < 5 ) Premy = Salary / 100 * 8; // если стаж > 3 лет но < 5
+        if(Expirience > 5) Premy = Salary / 100 * 15; // если стаж > 5 лет 
     }
 
-    public void Print()
+    public void Print() // метод вывода
     {
         Console.WriteLine($"Surname : {Surname}");
         Console.WriteLine($"Expirience is : {Expirience}");
@@ -55,11 +57,11 @@ class Worker
         Console.WriteLine($"Premy is : {Premy}");
     }
 
-    public void ReadToFile() {
+    public void RecordToFile() { // метод записи в файл
         string Info = "Surname is " + Surname + "'\n'Expirience is " + Expirience + "'\n'HourlyWag is " 
                       + HourlyWag + "'\n'HoursWorked is " + HoursWorked 
                       + "'\n'Salary is " + Salary + "'\n'Premy is " + Premy;
-        File.WriteAllText(@"C:\\Users\РомаЛиля\Desktop\C#Task11\test.txt",Info);
+        File.WriteAllText(@"C:\\Users\РомаЛиля\Desktop\C#Task11\test.txt",Info); // запись
     }
 }
 
